5b30c2bd95c28eb73c79f32c57ff2dde4808538e
Emanuele <emanuelepisaturo@hotmail.it>
2025-03-09 21:44:20 +0100

    aggiornamento login.js -> conversione a jquery

--- js/login.js
+++ js/login.js
@@ -1,43 +1,45 @@
-function togglePassword() {
-  var passwordField = $(`#password`);
-  var eyeIcon = $(`#eye-icon`);
-
-  if (passwordField.attr("type") === "password") {
-    passwordField.attr("type", "text");
-    eyeIcon.attr("src", "/assets/images/eye-open.png");
-  } else {
-    passwordField.attr("type", "password");
-    eyeIcon.attr("src", "/assets/images/eye-icon.png");
-  }
-}
-
-setTimeout(() => {
-  $(`#loader`).hide();
-  $(`#content`).show();
-}, 2000);
-
-function handleLogin() {
-  event.preventDefault();
-
-  // Nascondi il login form
-  const content = document.getElementById("loginCard");
-  content.style.display = "none";
-
-  // Mostra il menu hamburger
-  const openBtn = document.getElementById("nav-icon");
-  openBtn.style.display = "block";
-
-  // Aggiungi la classe 'active' alla sidebar al caricamento
-  const sidebar = document.getElementById("sidebar");
-  sidebar.classList.add("active");
-
-  openBtn.classList.add("open");
-}
-
 $(document).ready(function () {
-  // Gestisci il click sul pulsante per togglare il menu
-  $("#nav-icon").click(function () {
-    $(this).toggleClass("open"); // Aggiungi o rimuovi la "X"
-    $("#sidebar").toggleClass("active"); // Aggiungi o rimuovi la visibilità della sidebar
+  // Gestione del click sul pulsante di login
+  $("#loginBtn").click(function (event) {
+    event.preventDefault(); // Prevenire il comportamento di default del form
+
+    // Mostra il loader e nasconde il form di login
+    $("#loader").show();
+    $("#loginCard").hide();
+
+    // Usa setTimeout per fare le modifiche dopo 2 secondi
+    setTimeout(() => {
+      // Mostra il menu hamburger e la sidebar
+      $("#nav-icon").show().addClass("open");
+      $("#sidebar").addClass("active").show();
+
+      // Nascondi il loader
+      $("#loader").hide();
+    }, 2000);
   });
+
+  // Gestione del click per mostrare/nascondere la password
+  $("#eye-icon").click(function () {
+    var passwordField = $("#password");
+    var eyeIcon = $("#eye-icon");
+    if (passwordField.attr("type") === "password") {
+      passwordField.attr("type", "text");
+      eyeIcon.attr("src", "/assets/images/eye-open.png");
+    } else {
+      passwordField.attr("type", "password");
+      eyeIcon.attr("src", "/assets/images/eye-icon.png");
+    }
+  });
+
+  // Gestione del click per aprire/chiudere la navbar
+  $("#nav-icon").click(function () {
+    $(this).toggleClass("open"); // Aggiungi o rimuovi la classe "open"
+    $("#sidebar").toggleClass("active"); // Aggiungi o rimuovi la classe "active" per la sidebar
+  });
+
+  // Funzione timeout
+  setTimeout(() => {
+    $(`#loader`).hide(); // Nasconde l'elemento con id hider
+    $(`#content`).show(); // Mostra l'elemento con id shower
+  }, 2000);
 });
